3be0ccc32efaae3feed1112c040806a9be99b28a
samira fennoun <101664372+e2295531@users.noreply.github.com>
2023-04-18 12:53:19 -0400

    Update HeaderComponent.vue
    ajout 'mon cellier ' dans la barre de navigation

--- resources/js/components/HeaderComponent.vue
+++ resources/js/components/HeaderComponent.vue
@@ -24,6 +24,11 @@
                             :to="{ name: 'saq-produits' }"
                             >Obtenir produits SAQ</router-link
                         >
+                        <router-link
+                            class="nav-link inline-flex items-center px-1 pt-1 border-b-2 text-sm font-medium leading-5 text-gray-500 hover:text-gray-700 hover:border-gray-300 focus:outline-none focus:text-gray-700 focus:border-gray-300"
+                            :to="{ name: 'monCellier' }"
+                            >Mon Cellier</router-link
+                        >
                     </nav>
                 </div>
                 <div class="hidden sm:ml-6 sm:flex sm:items-center">
