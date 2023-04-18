2f18e6e9f1d5fc6e7c1e3693d6ea79156004b211
redouane.taleb1@gmail.com <redouane.taleb1@gmail.com>
2023-04-18 11:27:09 -0400

    résoudre les conflits de nomenclature

--- resources/js/views/LoginView.vue
+++ resources/js/views/LoginView.vue
@@ -1,29 +1,4 @@
 <template>
-<<<<<<< HEAD
-    <section>
-        <div class="background">
-            <form @submit.prevent="soumettre">
-                <h3>Nouvelle Utilisateur</h3>
-                <div>
-                    <label for="nom">Nom</label>
-                    <input type="text" v-model="name" id="nom" />
-                </div>
-                <div>
-                    <label for="courriel">Courriel</label>
-                    <input type="text" v-model="email" id="courriel" />
-                </div>
-                <div>
-                    <label for="mdp">Mot de passe</label>
-                    <input type="password" v-model="password" id="mdp" />
-                </div>
-                <div>
-                    <label for="conf-mdp">Confirmation du mot de passe</label>
-                    <input
-                        type="password"
-                        v-model="conf_password"
-                        id="conf-mdp"
-                    />
-=======
     <section class="mt-12">
         <div class="bg-[#F0EBEB] m-4 p-3 shadow-md rounded">
             <form @submit.prevent="connexion">
@@ -38,7 +13,6 @@
                 </div>
                 <div>
                     <button type="submit" class="mb-4 bg-vin_rouge text-vin_blanc rounded pt-1 pb-1 pr-5 pl-5">Se Connecter</button>
->>>>>>> upstream/main
                 </div>
             </form>
         </div>
@@ -46,22 +20,6 @@
 </template>
 
 <script>
-<<<<<<< HEAD
-export default {
-    data() {
-        return {
-            name: "",
-            email: "",
-            password: "",
-            conf_password: "",
-        };
-    },
-};
-methods: {
-    soumettre: store();
-}
-</script>
-=======
 
     export default {
         data() {
@@ -77,4 +35,3 @@ methods: {
         }
     }
 </script>
->>>>>>> upstream/main
