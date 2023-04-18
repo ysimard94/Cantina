7f4b76977ee4c85574b68a7a0e4dbf92c2ad5bd8
Simon-Olivier Blouin <soblouin2244@gmail.com>
2023-04-18 10:53:34 -0400

    Finalisation page logins
    Finalisation des pages de Connexion et Création d'uitilisateur

--- resources/js/views/LoginView.vue
+++ resources/js/views/LoginView.vue
@@ -4,15 +4,15 @@
             <form @submit.prevent="connexion">
                 <h3 class="mb-4 text-vin_rouge font-bold text-xl">Connexion</h3>
                 <div class="mb-4">
-                    <label for="courriel" class="block text-md text-left font-bold text-vin_rouge">Courriel</label>
+                    <label for="courriel" class="block text-lg text-left font-bold text-vin_rouge">Courriel</label>
                     <input type="text" v-model="courriel" id="courriel" class="w-full rounded">
                 </div>
                 <div class="mb-4">
-                    <label for="mdp" class="block text-md text-left font-bold text-vin_rouge">Mot de passe</label>
+                    <label for="mdp" class="block text-lg text-left font-bold text-vin_rouge">Mot de passe</label>
                     <input type="password" v-model="mdp" id="mdp" class="w-full rounded">
                 </div>
                 <div>
-                    <button type="submit" class="mb-4 bg-vin_rouge text-vin_blanc rounded pt-1 pb-1 pr-5 pl-5">Se Connecter</button>
+                    <button type="submit" class="mb-4 mt4 bg-vin_rouge text-vin_blanc rounded pt-1 pb-1 pr-5 pl-5">Se Connecter</button>
                 </div>
             </form>
         </div>
@@ -30,7 +30,7 @@
         }
     }
     methods: {
-        soumettre: () => {
+        connexion: () => {
 
         }
     }
--- resources/js/views/NewUserView.vue
+++ resources/js/views/NewUserView.vue
@@ -4,23 +4,23 @@
             <form @submit.prevent="soumettre">
                 <h3 class="mb-4 text-vin_rouge font-bold text-xl">Nouvelle Utilisateur</h3>
                 <div class="mb-4">
-                    <label for="nom" class="block text-md text-left font-bold text-vin_rouge">Nom</label>
-                    <input type="text" v-model="nom" id="nom" class="w-full rounded">
+                    <label for="nom" class="block text-lg text-left font-bold text-vin_rouge">Nom</label>
+                    <input type="text" v-model="nom" id="nom" class="w-full rounded pt-3">
                 </div>
                 <div class="mb-4">
-                    <label for="courriel" class="block text-md text-left font-bold text-vin_rouge">Courriel</label>
-                    <input type="text" v-model="courriel" id="courriel" class="w-full rounded">
+                    <label for="courriel" class="block text-lg text-left font-bold text-vin_rouge">Courriel</label>
+                    <input type="text" v-model="courriel" id="courriel" class="w-full rounded pt-3">
                 </div>
                 <div class="mb-4">
-                    <label for="mdp" class="block text-md text-left font-bold text-vin_rouge">Mot de passe</label>
-                    <input type="password" v-model="mdp" id="mdp" class="w-full rounded">
+                    <label for="mdp" class="block text-lg text-left font-bold text-vin_rouge">Mot de passe</label>
+                    <input type="password" v-model="mdp" id="mdp" class="w-full rounded pt-3">
                 </div>
                 <div class="mb-4">
-                    <label for="conf-mdp" class="block text-md text-left font-bold text-vin_rouge">Confirmation du mot de passe</label>
-                    <input type="password" v-model="conf_mdp" id="conf-mdp" class="w-full rounded">
+                    <label for="conf-mdp" class="block text-lg text-left font-bold text-vin_rouge">Confirmation du mot de passe</label>
+                    <input type="password" v-model="conf_mdp" id="conf-mdp" class="w-full rounded pt-3">
                 </div>
                 <div>
-                    <button type="submit" class="mb-4 bg-vin_rouge text-vin_blanc rounded pt-1 pb-1 pr-5 pl-5">S'inscrire</button>
+                    <button type="submit" class="mb-4 mt-4 bg-vin_rouge text-vin_blanc rounded pt-1 pb-1 pr-5 pl-5">S'inscrire</button>
                 </div>
             </form>
         </div>
