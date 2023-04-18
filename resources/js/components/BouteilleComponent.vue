b283576f6477307df7ab2dd5845251f64856a3b2
samira fennoun <101664372+e2295531@users.noreply.github.com>
2023-04-18 12:53:56 -0400

    Create BouteilleComponent.vue
    creation du composant 'Bouteillecomponent.vue'

---
+++ resources/js/components/BouteilleComponent.vue
@@ -0,0 +1,31 @@
+<template>
+    <div class="bg-white overflow-hidden shadow rounded-lg">
+        <img src="../../assets/vin.png" alt="Bottle image" class="w-16 h-32 object-cover mx-auto">
+        <div class="px-4 py-4">
+            <h4 class="text-lg font-medium text-gray-900">11th Hour Cellars Pinot Noir</h4>
+            <p class="text-gray-600">While the expression "11th hour" refers to a last-minute decision</p>
+            <div class="mt-4 flex justify-between items-center">
+                <div>
+                    <span class="text-gray-700 font-medium">$17.75</span>
+                    <span class="text-gray-400 font-medium">/ bouteille</span>
+                </div>
+                <div>
+                    <span class="text-gray-700 font-medium">United States</span>
+                    <span class="text-gray-400 font-medium">- 2018</span>
+                </div>
+            </div>
+        </div>
+        <div class="px-4 py-3 bg-gray-100">
+            <div class="flex justify-between items-center">
+                <div>
+                    <span class="text-gray-700 font-medium">rouge</span>
+                </div>
+                <div>
+                    <button class="text-sm text-red-600 hover:text-red-900 focus:outline-none"
+                        >Supprimer</button>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+
