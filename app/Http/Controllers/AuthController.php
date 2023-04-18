2959458eee9bdf65b376080a3a54e26cf19be3fa
redouane.taleb1@gmail.com <redouane.taleb1@gmail.com>
2023-04-18 11:45:53 -0400

    Ajout de la validation pour la creation d'un nouvelle utilisateur

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -29,11 +29,26 @@ class AuthController extends Controller
      */
     public function sauvegarder(Request $request)
     {
-        //
+        // Changer le nom de la variable de confirmation de mot de passe
+        $request->merge(['mdp_confirmation' => $request->get('conf_mdp')]);
+
+        // Valider les données
+        $request->validate([
+            'nom' => 'required',
+            'courriel' => 'required|email',
+            'mdp' => 'required|min:3',
+            'mdp_confirmation' => 'required|same:mdp'
+        ]);
+
+        // Créer l'utilisateur
         $utilisateur = new Utilisateur;
+
+        // Sauvegarder les données
         $utilisateur->fill($request->all());
         $utilisateur->mdp = Hash::make($request->mdp);
         $utilisateur->save();
+
+        // Retourner une réponse
         return response()->json(['message' => "L'Utilisateur a été créé avec succes"], 201);
     }
 
