748f33d7dc53eb275aee27a57b7f6a5e68bb927f
samira fennoun <101664372+e2295531@users.noreply.github.com>
2023-04-18 12:50:06 -0400

    Create CellierView.vue
    création de CellierView.vue

---
+++ resources/js/views/CellierView.vue
@@ -0,0 +1,23 @@
+<template>
+    <div class="container">
+        <h1>mon cellier</h1>
+        <div class="row">
+            <bouteille-card/>
+        </div>
+    </div>
+</template>
+
+<script>
+import BouteilleComponent from "@/components/BouteilleComponent.vue";
+
+export default {
+
+    components: {
+      'bouteille-card': BouteilleComponent
+
+    },
+    methods: {
+
+    },
+}
+</script>
