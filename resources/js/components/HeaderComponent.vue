b623e74c55b1e9ede298cdcc7cddb64307e70731
samira fennoun <101664372+e2295531@users.noreply.github.com>
2023-04-18 17:21:57 -0400

    récupération des données

--- resources/js/components/HeaderComponent.vue
+++ resources/js/components/HeaderComponent.vue
@@ -29,6 +29,11 @@
                             :to="{ name: 'monCellier' }"
                             >Mon Cellier</router-link
                         >
+                        <router-link
+                            class="nav-link inline-flex items-center px-1 pt-1 border-b-2 text-sm font-medium leading-5 text-gray-500 hover:text-gray-700 hover:border-gray-300 focus:outline-none focus:text-gray-700 focus:border-gray-300"
+                            :to="{ name: 'Catalogue.index' }"
+                            >Catalogue</router-link
+                        >
                     </nav>
                 </div>
                 <div class="hidden sm:ml-6 sm:flex sm:items-center">
