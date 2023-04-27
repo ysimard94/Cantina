e1568b45346eef5a706bef3244b47e824bf3560a
Simon-Olivier Blouin <soblouin2244@gmail.com>
2023-04-27 13:23:26 -0400

    Ajout de la barre de recherche du cellier

--- resources/js/views/CellierView.vue
+++ resources/js/views/CellierView.vue
@@ -1,65 +1,35 @@
 <template>
     <div class="container mx-auto px-2">
-        <div class="flex items-center">
+        <div class="flex items-center mx-auto px-2">
             <label for="select-cellier" class="mr-4 font-medium text-gray-700">
-                celliers :</label
-            >
-            <select
-                id="select-cellier"
-                @change="handleChangerCellier"
-                v-model="cellierActif"
-                class="mr-4 p-2 rounded-md shadow-sm border-gray-300 focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50"
-            >
+                celliers :</label>
+            <select id="select-cellier" @change="handleChangerCellier" v-model="cellierActif"
+                class="mr-4 p-2 rounded-md shadow-sm border-gray-300 focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50">
                 <option disabled>-- Sélectionner un cellier --</option>
-                <option
-                    v-for="(cellier, index) in celliers"
-                    :key="index"
-                    :value="cellier"
-                >
+                <option v-for="(cellier, index) in celliers" :key="index" :value="cellier">
                     {{ cellier.nom }}
                 </option>
             </select>
             <router-link to="/ajouter-cellier">
                 <button
-                    class="px-4 py-2 rounded-md bg-vin_rouge text-white hover:bg-vin_blanc focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
-                >
-                    <svg
-                        class="w-6 h-6"
-                        fill="none"
-                        stroke="currentColor"
-                        viewBox="0 0 24 24"
-                        xmlns="http://www.w3.org/2000/svg"
-                    >
-                        <path
-                            stroke-linecap="round"
-                            stroke-linejoin="round"
-                            stroke-width="2"
-                            d="M12 6v6m0 0v6m0-6h6m-6 0H6"
-                        ></path>
+                    class="px-4 py-2 rounded-md bg-vin_rouge text-white hover:bg-vin_blanc focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">
+                    <svg class="w-6 h-6" fill="none" stroke="currentColor" viewBox="0 0 24 24"
+                        xmlns="http://www.w3.org/2000/svg">
+                        <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+                            d="M12 6v6m0 0v6m0-6h6m-6 0H6"></path>
                     </svg>
                     <span class="sr-only">Ajouter un cellier</span>
                 </button>
             </router-link>
         </div>
 
-        <div
-            class="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 gap-8 h-full"
-        >
-            <div
-                class="bg-bg_rose flex flex-col overflow-hidden shadow rounded-lg py-2 m-2 h-full"
-                v-for="bouteille in bouteilles"
-                :key="bouteille.id"
-            >
-                <img
-                    :src="bouteille.photo"
-                    :alt="bouteille.nom"
-                    class="w-1/3 mx-auto"
-                />
+        <div class="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 gap-8 h-full">
+            <div class="bg-bg_rose flex flex-col overflow-hidden shadow rounded-lg py-2 m-2 h-full"
+                v-for="bouteille in bouteilles" :key="bouteille.id">
+                <img :src="bouteille.photo" :alt="bouteille.nom" class="w-1/3 mx-auto" />
 
                 <div class="px-4 pt-4 flex flex-col justify-between">
-                    <h4
-                        class="text-lg font-serif font-semibold text-vin-rouge text-left h-[67px] leading-tight"
-                    >
+                    <h4 class="text-lg font-serif font-semibold text-vin-rouge text-left h-[67px] leading-tight">
                         {{ bouteille.nom }}
                     </h4>
                     <div class="mt-4 flex justify-between items-center">
@@ -72,15 +42,11 @@
                             </div>
                         </div>
                         <div>
-                            <div
-                                class="text-gray-700 font-medium mr-2 text-right"
-                            >
+                            <div class="text-gray-700 font-medium mr-2 text-right">
                                 Note
                             </div>
                             <div class="flex items-center">
-                                <div
-                                    class="text-gray-600 font-medium text-right"
-                                >
+                                <div class="text-gray-600 font-medium text-right">
                                     {{ bouteille.note }}%({{
                                         bouteille.nbr_notes
                                     }}
@@ -94,10 +60,8 @@
                 <div class="mt-auto">
                     <div class="pb-2 px-4 font-sans">
                         <div class="flex justify-center">
-                            <button
-                                @click="supprimerBouteille(bouteille.id)"
-                                class="bg-vin_rouge rounded-md w-full text-sm text-vin_blanc hover:text-white focus:outline-none px-2 py-2"
-                            >
+                            <button @click="supprimerBouteille(bouteille.id)"
+                                class="bg-vin_rouge rounded-md w-full text-sm text-vin_blanc hover:text-white focus:outline-none px-2 py-2">
                                 supprimer
                             </button>
                         </div>
@@ -106,33 +70,30 @@
                 </div>
             </div>
         </div>
+        <div class="flex items-center mx-auto p-2">
+            <form @submit.prevent="" class="w-full">
+                <label for="rechercheCellier"></label>
+                <input type="text" id="rechercheCellier" v-model="rechercheCellier"
+                    class="w-full rounded pt-2 pb-2 pl-1 pr-1">
+                <span class="absolute right-0"><img src="@assets/search_FILL1_wght400_GRAD0_opsz40.svg" alt="Recherche"
+                        class="mr-4"></span>
+            </form>
+        </div>
 
-        <router-link
-            :to="{
-                name: 'ajouter-bouteille',
-                params: { cellierId: cellierActif.id },
-            }"
-        >
+        <router-link :to="{
+                    name: 'ajouter-bouteille',
+                    params: { cellierId: cellierActif.id },
+                }">
             <div class="fixed bottom-0 right-0 mb-8 mr-8">
-                <button
-                    class="bg-vin_blanc hover:bg-gray-700 text-white font-bold py-4 px-4 rounded-full"
-                >
-                    <svg
-                        class="w-6 h-6"
-                        fill="none"
-                        stroke="currentColor"
-                        viewBox="0 0 24 24"
-                        xmlns="http://www.w3.org/2000/svg"
-                    >
-                        <path
-                            stroke-linecap="round"
-                            stroke-linejoin="round"
-                            stroke-width="2"
-                            d="M12 6v6m0 0v6m0-6h6m-6 0H6"
-                        ></path>
+                <button class="bg-vin_blanc hover:bg-gray-700 text-white font-bold py-4 px-4 rounded-full">
+                    <svg class="w-6 h-6" fill="none" stroke="currentColor" viewBox="0 0 24 24"
+                        xmlns="http://www.w3.org/2000/svg">
+                        <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+                            d="M12 6v6m0 0v6m0-6h6m-6 0H6"></path>
                     </svg>
-                </button></div
-        ></router-link>
+                </button>
+            </div>
+        </router-link>
     </div>
 </template>
 
@@ -145,22 +106,22 @@ export default {
     components: {
         "bouteille-card": BouteilleComponent,
     },
-    data() {
+    data () {
         return {
             bouteilles: [],
             celliers: [],
             cellierActif: { id: 0, nom: "Aucun cellier" },
         };
     },
-    async mounted() {
+    async mounted () {
         await this.fetchCelliers();
         await this.fetchBouteillesCellier();
     },
     methods: {
-        handleChangerCellier() {
+        handleChangerCellier () {
             this.fetchBouteillesCellier();
         },
-        async fetchBouteillesCellier() {
+        async fetchBouteillesCellier () {
             try {
                 const response =
                     await BouteilleDataService.getBouteillesByCellierId(
@@ -172,7 +133,7 @@ export default {
                 console.log(error);
             }
         },
-        async supprimerBouteille(bouteilleId) {
+        async supprimerBouteille (bouteilleId) {
             console.log(bouteilleId);
             try {
                 await CellierDataService.supprimerBouteilleCellier(
@@ -186,7 +147,7 @@ export default {
                 console.log(error);
             }
         },
-        async fetchCelliers() {
+        async fetchCelliers () {
             try {
                 const response = await CellierDataService.getAll();
                 this.celliers = response.data;
