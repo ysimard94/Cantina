7607246625f920006b7b65c029b51eb672ef56fb
Yannick Simard <yannick.simard94@gmail.com>
2023-04-27 22:43:17 -0400

    Ajout d'animations et couleur de fond pour la page active

--- resources/js/components/FooterComponent.vue
+++ resources/js/components/FooterComponent.vue
@@ -4,21 +4,27 @@
             <!-- Si l'utilisateur est connecté, montrer le menu de navigation -->
             <div class="flex justify-around" v-if="estConnecter">
                 <router-link class="w-full" :to="{ name: 'mes-celliers' }">
-                    <button class="material-symbols-outlined text-white text-5xl px-5 py-3 actif w-full">
+                    <button :class="{ 'actif': pageActive === 0 }"
+                        class="material-symbols-outlined text-white text-5xl px-5 py-3 w-full"
+                        @click="fermerMenu(); pageActive = 0">
                         home
                     </button>
                 </router-link>
-                <router-link class="w-full" :to="{ name: 'modifier-utilisateur' }">
-                    <button class="material-symbols-outlined text-white text-5xl px-5 py-3 w-full">
+                <router-link class="w-full" :to=" { name: 'modifier-utilisateur' } ">
+                    <button :class=" { 'actif': pageActive === 1 } "
+                        class="material-symbols-outlined text-white text-5xl px-5 py-3 w-full"
+                        @click=" fermerMenu(); pageActive = 1 ">
                         account_circle
                     </button>
                 </router-link>
-                <button class="material-symbols-outlined text-white text-5xl w-full" @click="afficherMenu">
+                <!-- Si la page active est déjà celle-ci, le status de pageActive est réinitialisée à sa valeur initiale -->
+                <button :class=" { 'actif': pageActive === 2 } " class="material-symbols-outlined text-white text-5xl w-full"
+                    @click="afficherMenu(); pageActive = pageActive === 2 ? -1 : 2 ">
                     menu
                 </button>
             </div>
             <!-- Sinon montrer le logo -->
-            <img src="@assets/vino-logo.png" alt="Logo Vino" v-if="!estConnecter" class="w-[48px] py-3 m-auto">
+            <img src="@assets/vino-logo.png" alt="Logo Vino" v-if=" !estConnecter " class="w-[48px] py-3 m-auto">
         </div>
     </footer>
 </template>
@@ -26,6 +32,9 @@
 <script>
 export default {
     name: "FooterComponent",
+    props: {
+        pageActive: Number,
+    },
     computed: {
         estConnecter() {
             console.log(this.$store.state.session.utilisateur_id);
@@ -36,6 +45,9 @@ export default {
         afficherMenu() {
             this.$emit('toggle-menu')
         },
+        fermerMenu() {
+            this.$emit("fermer-menu");
+        },
     },
 };
 </script>
@@ -44,5 +56,5 @@ export default {
 /* Add any necessary styles here */
 .actif {
     background-color: #5c1b38;
-}
-</style>
+    transition: background-color 300ms ease-in-out;
+}</style>
