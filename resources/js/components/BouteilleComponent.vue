dbde9fdcb0d4d1d82ba6d3a5c0693ecf6bc8ee2a
redouane.taleb1@gmail.com <redouane.taleb1@gmail.com>
2023-05-03 10:04:32 -0400

    Résolution du conflit de fusion

--- resources/js/components/BouteilleComponent.vue
+++ resources/js/components/BouteilleComponent.vue
@@ -1,12 +1,12 @@
 <template>
-
-<!--
-    <div class="bg-bg_rose flex flex-col overflow-hidden shadow rounded-lg h-full" v-for="bouteille in bouteilles"
+    <!-- <div class="bg-bg_rose flex flex-col overflow-hidden shadow rounded-lg h-full" v-for="bouteille in bouteilles"
         :key="bouteille.id">
         <div class="flex items-start">
             <div class="w-1/3">
                 <div class="relative ">
                     <div class="text-center text-white font-bold font-sans" :class="categorieBgColor(bouteille.categorie.nom)">
+                  
+                    
                     -->
 
     <div
@@ -21,7 +21,6 @@
                         class="text-center text-white font-bold"
                         :class="categorieBgColor(bouteille.categorie.nom)"
                     >
-
                         {{ bouteille.categorie.nom }}
                     </div>
                     <img
@@ -33,14 +32,15 @@
             </div>
 
             <div class="px-4 pt-3 w-2/3 flex flex-col h-full">
-                <h4 class="font-serif font-semibold text-vin-rouge text-left h-[67px] leading-tight">
-
-<!--
-            <div class="px-4 pt-4 w-2/3">
                 <h4
-                    class="text-lg font-serif font-semibold text-vin-rouge text-left h-[67px] leading-tight"
+                    class="font-serif font-semibold text-vin-rouge text-left h-[67px] leading-tight"
                 >
-                -->
+                    <!--
+                <div class="px-4 pt-4 w-2/3">
+                    <h4
+                        class="text-lg font-serif font-semibold text-vin-rouge text-left h-[67px] leading-tight"
+                    >
+                    -->
 
                     {{ bouteille.nom }}
                 </h4>
@@ -50,59 +50,76 @@
                             <div class="font-medium text-left">
                                 {{ bouteille.pays.nom }}
                             </div>
-                            <div class="font-medium text-left">
-                                Qté: {{ bouteille.pivot.quantite }}
-                            </div>
-                        </div>
-                        <div>
-                            <div class="text-gray-700 font-medium mr-2 text-right">
-                                Note
-                            </div>
-                            <div class="flex items-center">
-                                <div class="text-gray-600 font-medium text-right">
-                                    ({{ bouteille.nbr_notes }} avis)
+                            <div
+                                class="w-full flex items-center justify-between"
+                            >
+                                <div class="font-medium text-left">
+                                    Qté: {{ bouteille.pivot.quantite }}
+                                </div>
+                                <!-- Notes -->
+                                <div class="text-gray-700 font-medium mr-2">
+                                    <div class="flex items-center">
+                                        <template v-for="i in 5" :key="i">
+                                            <svg
+                                                :class="{
+                                                    'text-yellow-500':
+                                                        bouteille.note / 20 >=
+                                                        i,
+                                                }"
+                                                class="w-4 h-4 fill-current"
+                                                xmlns="http://www.w3.org/2000/svg"
+                                                viewBox="0 0 20 20"
+                                            >
+                                                <path
+                                                    d="M10 15.95L4.43 19.7a.5.5 0 0 1-.74-.53l.98-5.71L.21 8.53a.5.5 0 0 1 .28-.85l5.75-.83L9.1 2.27a.5.5 0 0 1 .9 0l2.87 5.8 5.75.83a.5.5 0 0 1 .28.85l-4.45 4.35 1 5.71a.5.5 0 0 1-.74.53L10 15.95z"
+                                                />
+                                            </svg>
+                                        </template>
+                                    </div>
                                 </div>
                             </div>
                         </div>
+                        <!-- <div>
+                            <div
+                                class="text-gray-700 font-medium mr-2 text-right"
+                            >
+                                Note
+                            </div>
+                            <div class="flex items-center">
+                                <div
+                                    class="text-gray-600 font-medium text-right"
+                                >
+                                    ({{ bouteille.nbr_notes }} avis)
+                                </div>
+                            </div>
+                        </div> -->
                     </div>
                     <div>
-
                         <div class="pb-2 font-sans flex justify-end mt-2">
-                            <button v-if="!bouteille.code_saq"
-                                class="material-symbols-outlined w-10 h-10 rounded-lg  text-white font-semibold bg-vin-blanc mr-2">
+                            <button
+                                v-if="!bouteille.code_saq"
+                                class="material-symbols-outlined w-10 h-10 rounded-lg text-white font-semibold bg-vin-blanc mr-2"
+                            >
                                 edit
                             </button>
-                            <div v-if="bouteille.code_saq" class="w-[32px] my-auto mr-2">
+                            <div
+                                v-if="bouteille.code_saq"
+                                class="w-[32px] my-auto mr-2"
+                            >
                                 <img src="@assets/saq.svg" alt="SAQ Icon" />
                             </div>
                             <button
-                                class="material-symbols-outlined w-10 h-10 rounded-lg  text-white font-semibold bg-vin-rouge ml-2">
+                                class="material-symbols-outlined w-10 h-10 rounded-lg text-white font-semibold bg-vin-rouge ml-2"
+                            >
                                 delete
                             </button>
-                        <div class="text-gray-700 font-medium mr-2 text-right">
-                            <div class="flex items-center">
-                                <template v-for="i in 5" :key="i">
-                                    <svg
-                                        :class="{
-                                            'text-yellow-500':
-                                                bouteille.note / 20 >= i,
-                                        }"
-                                        class="w-4 h-4 fill-current"
-                                        xmlns="http://www.w3.org/2000/svg"
-                                        viewBox="0 0 20 20"
-                                    >
-                                        <path
-                                            d="M10 15.95L4.43 19.7a.5.5 0 0 1-.74-.53l.98-5.71L.21 8.53a.5.5 0 0 1 .28-.85l5.75-.83L9.1 2.27a.5.5 0 0 1 .9 0l2.87 5.8 5.75.83a.5.5 0 0 1 .28.85l-4.45 4.35 1 5.71a.5.5 0 0 1-.74.53L10 15.95z"
-                                        />
-                                    </svg>
-                                </template>
-                            </div>
                         </div>
+
                         <!--<div class="flex items-center">Note
                         </div> #}-->
                     </div>
                 </div>
-                <div class="mt-auto">
+                <!-- <div class="mt-auto">
                     <div class="pb-2 px-4 font-sans flex justify-center mt-2">
                         <router-link
                             :to="{
@@ -132,7 +149,7 @@
                             delete
                         </button>
                     </div>
-                </div>
+                </div> -->
             </div>
         </div>
     </div>
