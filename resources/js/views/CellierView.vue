dd15b5ed058f77c769d7e0f80caa5c6e2d87eed7
redouane.taleb1@gmail.com <redouane.taleb1@gmail.com>
2023-05-02 23:02:23 -0400

    Réinistialiser les filtres et tris en cliquant sur le bouton refresh et  au changement d'un cellier

--- resources/js/views/CellierView.vue
+++ resources/js/views/CellierView.vue
@@ -1,56 +1,95 @@
 <template>
     <div class="container mx-auto px-2">
-        <div class="rounded-md mt-4 grid grid-cols-1 md:grid-cols-3 gap-4 items-center mx-2 px-2 py-4 bg-bg-rose">
-            <label for="select-cellier" class="md:col-span-1 font-medium text-gray-700 text-left">Mes celliers</label>
+        <div
+            class="rounded-md mt-4 grid grid-cols-1 md:grid-cols-3 gap-4 items-center mx-2 px-2 py-4 bg-bg-rose"
+        >
+            <label
+                for="select-cellier"
+                class="md:col-span-1 font-medium text-gray-700 text-left"
+                >Mes celliers</label
+            >
             <div class="md:col-span-2 flex justify-between items-center">
-                <select id="select-cellier" @change="handleChangerCellier" v-model="cellierActif"
-                    class="mr-2 p-2 font-semibold w-full rounded-md shadow-sm border-gray-300 focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50">
+                <select
+                    id="select-cellier"
+                    @change="handleChangerCellier"
+                    v-model="cellierActif"
+                    class="mr-2 p-2 font-semibold w-full rounded-md shadow-sm border-gray-300 focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50"
+                >
                     <option disabled>-- Sélectionner un cellier --</option>
-                    <option v-for="(cellier, index) in celliers" :key="index" :value="cellier">
+                    <option
+                        v-for="(cellier, index) in celliers"
+                        :key="index"
+                        :value="cellier"
+                    >
                         {{ cellier.nom }}
                     </option>
                 </select>
                 <div class="flex">
-                    <button class="material-symbols-outlined w-9 px-2 py-1 font-semibold text-vin-rouge"
-                        @click="handleEditButton">
+                    <button
+                        class="material-symbols-outlined w-9 px-2 py-1 font-semibold text-vin-rouge"
+                        @click="handleEditButton"
+                    >
                         edit
                     </button>
-                    <button class="material-symbols-outlined w-9 px-2 py-1 font-semibold text-vin-rouge"
-                        @click="handleAddButton">
+                    <button
+                        class="material-symbols-outlined w-9 px-2 py-1 font-semibold text-vin-rouge"
+                        @click="handleAddButton"
+                    >
                         add
                     </button>
                 </div>
             </div>
             <div v-if="showAjouterCellier" class="md:col-span-3 mt-4">
-                <AjouterCellierComponent @close="showAjouterCellier = false" @nouveau-cellier="ajoutCellier" />
+                <AjouterCellierComponent
+                    @close="showAjouterCellier = false"
+                    @nouveau-cellier="ajoutCellier"
+                />
             </div>
             <div v-if="showModifierCellier" class="md:col-span-3 mt-4">
-                <ModifierCellierComponent :cellier="cellierActif" @close="showModifierCellier = false"
-                    @cellier-modifie="mettreAJourCellier" @cellier-supprime="supprimerCellier" />
+                <ModifierCellierComponent
+                    :cellier="cellierActif"
+                    @close="showModifierCellier = false"
+                    @cellier-modifie="mettreAJourCellier"
+                    @cellier-supprime="supprimerCellier"
+                />
             </div>
         </div>
-
 
         <div class="flex items-center mx-auto p-2">
             <form @submit.prevent="" class="w-full">
                 <label for="rechercheCellier" class="relative">
-                    <input type="text" id="rechercheCellier" v-model="rechercheCellier"
-                        class="w-full rounded pt-2 pb-2 pl-1 pr-10" placeholder="Rechercher dans le cellier" />
-                    <buttons class="absolute right-0 pl-2" @click="rechercheBouteillesCellier"><span
-                            class="material-symbols-outlined text-4xl font-medium">
+                    <input
+                        type="text"
+                        id="rechercheCellier"
+                        v-model="rechercheCellier"
+                        class="w-full rounded pt-2 pb-2 pl-1 pr-10"
+                        placeholder="Rechercher dans le cellier"
+                    />
+                    <buttons
+                        class="absolute right-0 pl-2"
+                        @click="rechercheBouteillesCellier"
+                        ><span
+                            class="material-symbols-outlined text-4xl font-medium"
+                        >
                             search
-                        </span></buttons>
+                        </span></buttons
+                    >
                 </label>
             </form>
         </div>
-        <div v-if="cellierActif.id === 0" class="w-full flex flex-col items-center justify-center h-full">
+        <div
+            v-if="cellierActif.id === 0"
+            class="w-full flex flex-col items-center justify-center h-full"
+        >
             <p>
                 Vous n'avez pas encore enregistré de cellier. Pour commencer,
                 cliquez sur le bouton ci-dessous pour ajouter un nouveau
                 cellier.
             </p>
-            <button class="material-symbols-outlined w-9 px-2 py-1 font-semibold text-vin-rouge text-center"
-                @click="handleAddButton">
+            <button
+                class="material-symbols-outlined w-9 px-2 py-1 font-semibold text-vin-rouge text-center"
+                @click="handleAddButton"
+            >
                 add
             </button>
             <p>
@@ -58,63 +97,101 @@
                 les suivre facilement
             </p>
         </div>
-        <div v-else class="flex flex-col items-center justify-center h-full px-2">
+        <div
+            v-else
+            class="flex flex-col items-center justify-center h-full px-2"
+        >
             <div v-if="bouteillesAffiches && bouteillesAffiches.length > 0">
                 <!-- Section pour filtre et tri -->
                 <div class="md:col-span-2 flex justify-between items-center">
-                    <button class="mr-auto rounded pt-2 pb-2 pl-1 pr-10" @click="estOuvertFiltre = !estOuvertFiltre">
-                        <span class="material-symbols-outlined text-4xl font-medium text-vin-rouge">tune</span>
+                    <button
+                        class="mr-auto rounded pt-2 pb-2 pl-1 pr-10"
+                        @click="estOuvertFiltre = !estOuvertFiltre"
+                    >
+                        <span
+                            class="material-symbols-outlined text-4xl font-medium text-vin-rouge"
+                            >tune</span
+                        >
                     </button>
-                    <transition enter-active-class="transition duration-500 ease-in-out transform"
-                        enter-from-class="-translate-x-full" enter-to-class="translate-x-0"
-                        leave-active-class="transition duration-500 ease-in-out transform" leave-from-class="translate-x-0"
-                        leave-to-class="-translate-x-full">
-                        <FiltreComponent v-if="estOuvertFiltre" @filtrer-bouteilles="filtrerBouteilles"
-                            @fermer-filtre="fermerFiltre" :bouteilles="bouteilles"
-                            class="w-full fixed inset-0 mb-15 z-50" />
+                    <transition
+                        enter-active-class="transition duration-500 ease-in-out transform"
+                        enter-from-class="-translate-x-full"
+                        enter-to-class="translate-x-0"
+                        leave-active-class="transition duration-500 ease-in-out transform"
+                        leave-from-class="translate-x-0"
+                        leave-to-class="-translate-x-full"
+                    >
+                        <FiltreComponent
+                            v-if="estOuvertFiltre"
+                            @filtrer-bouteilles="filtrerBouteilles"
+                            @fermer-filtre="fermerFiltre"
+                            :bouteilles="bouteilles"
+                            class="w-full fixed inset-0 mb-15 z-50"
+                        />
                     </transition>
-                    <button class="rounded" @click="reinitialisationBouteilles()">
-                        <span class="material-symbols-outlined text-4xl font-medium text-vin-rouge pr-1">
+                    <button
+                        class="rounded"
+                        @click="reinitialisationBouteilles()"
+                    >
+                        <span
+                            class="material-symbols-outlined text-4xl font-medium text-vin-rouge pr-1"
+                        >
                             refresh
                         </span>
                     </button>
                     <button class="rounded" @click="triCellier">
-                        <span class="material-symbols-outlined text-4xl font-medium text-vin-rouge pr-1">
+                        <span
+                            class="material-symbols-outlined text-4xl font-medium text-vin-rouge pr-1"
+                        >
                             sort_by_alpha
                         </span>
                     </button>
                 </div>
-                <div class="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 gap-8 h-full">
+                <div
+                    class="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 gap-8 h-full"
+                >
                     <BouteilleComponent
-                        :bouteilles="bouteillesAffiches" :cellierId="cellierActif.id" />
-
+                        :bouteilles="bouteillesAffiches"
+                        :cellierId="cellierActif.id"
+                    />
                 </div>
             </div>
-            <div v-else class="w-full flex flex-col items-center justify-center h-full">
+            <div
+                v-else
+                class="w-full flex flex-col items-center justify-center h-full"
+            >
                 <p>
                     Félicitations, votre cellier a été ajouté avec succès ! Pour
                     commencer à suivre vos bouteilles de vin, vous pouvez
                     ajouter une bouteille dès maintenant en cliquant sur le
                     bouton Plus(+) ci-dessous
                 </p>
-
             </div>
-
-
-
-
         </div>
 
-        <router-link :to="{
+        <router-link
+            :to="{
                 name: 'ajouter-bouteille',
                 params: { cellierId: cellierActif.id },
-            }">
+            }"
+        >
             <div class="fixed bottom-[72px] right-0 mb-8 mr-8">
-                <button class="bg-vin_blanc hover:bg-gray-700 text-white font-bold py-4 px-4 rounded-full">
-                    <svg class="w-6 h-6" fill="none" stroke="currentColor" viewBox="0 0 24 24"
-                        xmlns="http://www.w3.org/2000/svg">
-                        <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
-                            d="M12 6v6m0 0v6m0-6h6m-6 0H6"></path>
+                <button
+                    class="bg-vin_blanc hover:bg-gray-700 text-white font-bold py-4 px-4 rounded-full"
+                >
+                    <svg
+                        class="w-6 h-6"
+                        fill="none"
+                        stroke="currentColor"
+                        viewBox="0 0 24 24"
+                        xmlns="http://www.w3.org/2000/svg"
+                    >
+                        <path
+                            stroke-linecap="round"
+                            stroke-linejoin="round"
+                            stroke-width="2"
+                            d="M12 6v6m0 0v6m0-6h6m-6 0H6"
+                        ></path>
                     </svg>
                 </button>
             </div>
@@ -129,6 +206,7 @@ import BouteilleDataService from "@/services/BouteilleDataService.js";
 import CellierDataService from "@/services/CellierDataService.js";
 import AjouterCellierComponent from "@/components/AjouterCellierComponent.vue";
 import ModifierCellierComponent from "@/components/ModifierCellierComponent.vue";
+import { mapMutations } from "vuex";
 export default {
     components: {
         BouteilleComponent,
@@ -177,6 +255,9 @@ export default {
         },
         handleChangerCellier() {
             this.fetchBouteillesCellier();
+
+            // Réinitialiser les filtres et tris au changement du cellier
+            this.reinitialisationBouteilles();
         },
         async fetchBouteillesCellier() {
             try {
@@ -304,7 +385,10 @@ export default {
             if (this.filteredBouteilles.length > 0) {
                 this.filteredBouteilles = [];
             }
+            // Mettre à jour les valeurs du filtre
+            this.setCellierFiltreValeurs({});
         },
+        ...mapMutations(["setCellierFiltreValeurs"]),
     },
 };
 </script>
