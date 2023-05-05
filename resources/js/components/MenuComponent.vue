b1bdb3fa8cb8807495c80612d4f9381a64782cb7
redouane.taleb1@gmail.com <redouane.taleb1@gmail.com>
2023-05-05 18:02:57 -0400

    Cacher la barre de recherche dans le header si l'utilisateur ne s'est pas connecté

--- resources/js/components/MenuComponent.vue
+++ resources/js/components/MenuComponent.vue
@@ -1,32 +1,58 @@
 <template>
-  <div :class="{ 'transform translate-x-full opacity-0': !menuOuvert, 'transform translate-x-0 opacity-100': menuOuvert }" class="fixed inset-0 transition-all duration-500 mb-[60px]">
-    <div class="w-full h-full bg-black bg-opacity-90 flex">
-      <nav class="h-full w-full flex flex-col justify-center">
-        <router-link :to="{ name: 'saq-produits' }" class="w-full text-2xl text-white py-2 mt-auto" @click="fermerMenu">Obtenir les bouteilles SAQ</router-link>
-        <router-link :to="{ name: 'catalogue.index' }" class="w-full text-2xl text-white py-2" @click="fermerMenu">Catalogue</router-link>
-        <router-link :to="{ name: 'a-propos' }" class="w-full text-2xl text-white py-2" @click="fermerMenu">À propos</router-link>
-        <router-link :to="{ name: 'deconnexion' }" class="w-full text-2xl text-white py-2 mb-auto" @click="fermerMenu">Déconnexion</router-link>
-      </nav>
+    <div
+        :class="{
+            'transform translate-x-full opacity-0': !menuOuvert,
+            'transform translate-x-0 opacity-100': menuOuvert,
+        }"
+        class="fixed inset-0 transition-all duration-500 mb-[60px]"
+    >
+        <div class="w-full h-full bg-black bg-opacity-90 flex">
+            <nav class="h-full w-full flex flex-col justify-center">
+                <!-- <router-link :to="{ name: 'saq-produits' }" class="w-full text-2xl text-white py-2 mt-auto" @click="fermerMenu">Obtenir les bouteilles SAQ</router-link>
+        <router-link :to="{ name: 'catalogue.index' }" class="w-full text-2xl text-white py-2" @click="fermerMenu">Catalogue</router-link> -->
+                <router-link
+                    :to="{ name: 'a-propos' }"
+                    class="w-full text-2xl text-white py-2 mt-auto"
+                    @click="fermerMenu"
+                    >À propos</router-link
+                >
+                <router-link
+                    :to="{ name: 'deconnexion' }"
+                    class="w-full text-2xl text-white py-2 mb-auto"
+                    @click="fermerMenu"
+                    >Déconnexion</router-link
+                >
+            </nav>
+        </div>
+        <button
+            :class="{
+                'transform -rotate-180 opacity-0': !menuOuvert,
+                'transform opacity-100': menuOuvert,
+            }"
+            class="material-symbols-outlined text-white text-6xl absolute top-5 left-5 transition-all duration-500"
+            @click="
+                fermerMenu();
+                reinitialiserPage();
+            "
+        >
+            close
+        </button>
     </div>
-    <button :class="{ 'transform -rotate-180 opacity-0': !menuOuvert, 'transform opacity-100': menuOuvert }" class="material-symbols-outlined text-white text-6xl absolute top-5 left-5 transition-all duration-500" @click="fermerMenu(); reinitialiserPage()">
-      close
-    </button>
-  </div>
 </template>
 
 <script>
 export default {
-  name: "MenuComponent",
-  props: {
-    menuOuvert: Boolean
-  },
-  methods: {
-    fermerMenu() {
-      this.$emit("fermer-menu");
+    name: "MenuComponent",
+    props: {
+        menuOuvert: Boolean,
     },
-    reinitialiserPage() {
-      this.$emit("reinitialiser-page");
+    methods: {
+        fermerMenu() {
+            this.$emit("fermer-menu");
+        },
+        reinitialiserPage() {
+            this.$emit("reinitialiser-page");
+        },
     },
-  },
 };
 </script>
