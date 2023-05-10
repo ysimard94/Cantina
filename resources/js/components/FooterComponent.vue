4e50b7433faf90f7b4c417629306be0c87172818
Yannick Simard <yannick.simard94@gmail.com>
2023-05-10 19:56:30 -0400

    Modification de la navigation

--- resources/js/components/FooterComponent.vue
+++ resources/js/components/FooterComponent.vue
@@ -5,22 +5,43 @@
             <div class="flex justify-around" v-if="estConnecter">
                 <router-link class="w-full" :to="{ name: 'mes-celliers' }">
                     <button :class="{ 'actif': pageActive === 0 }"
-                        class="material-symbols-outlined text-white text-3xl px-5 py-3 w-full"
+                        class="material-symbols-outlined text-white text-2xl px-5 py-3 w-full"
                         @click="fermerMenu(); changerPageActive(0)">
                         home
                     </button>
                 </router-link>
                 <router-link class="w-full" :to="{ name: 'modifier-utilisateur' }">
                     <button :class="{ 'actif': pageActive === 1 }"
-                        class="material-symbols-outlined text-white text-3xl px-5 py-3 w-full"
+                        class="material-symbols-outlined text-white text-2xl px-5 py-3 w-full"
                         @click=" fermerMenu(); changerPageActive(1)">
                         account_circle
                     </button>
                 </router-link>
-                <!-- Si la page active est déjà celle-ci, le status de pageActive est réinitialisée à sa valeur initiale -->
-                <button :class="{ 'actif': pageActive === 2 }"
-                    class="material-symbols-outlined text-white text-3xl w-full"
+                <div class="w-20" :class="{ 'actif': pageActive === 1 }"></div>
+                <button :class="{ 'bg-gray-700 rotate-45': pageActive === 2 }"
+                    class="material-symbols-outlined bg-vin-blanc hover:bg-gray-700 text-white font-bold p-4 rounded-full transform transition-all duration-00 absolute bottom-0 text-3xl"
                     @click="afficherMenu(); changerPageActive(pageActive === 2 ? -1 : 2)">
+                    add
+                </button>
+                <div class=" w-20" :class="{ 'actif': pageActive === 3 }"></div>
+                <router-link class="w-full" 
+                    :to="{
+                    name: 'archive-bouteille',
+                    params: {
+                        utilisateurId: this.$store.getters.session.utilisateur_id,
+                    },
+                    }"
+                >
+                    <button :class="{ 'actif': pageActive === 3 }"
+                        class="material-symbols-outlined text-white text-2xl px-5 py-3 w-full"
+                        @click=" fermerMenu(); changerPageActive(3)">
+                        menu_book
+                    </button>
+                </router-link>
+                <!-- Si la page active est déjà celle-ci, le status de pageActive est réinitialisée à sa valeur initiale -->
+                <button :class="{ 'actif': pageActive === 4 }"
+                    class="material-symbols-outlined text-white text-2xl w-full"
+                    @click="afficherMenu(); changerPageActive(pageActive === 4 ? -1 : 4)">
                     menu
                 </button>
             </div>
